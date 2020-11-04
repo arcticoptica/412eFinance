057ebbaf2a52024d96f157e75c13a537ebb96211
Arcticoptica <phillipliu1@gmail.com>
2020-11-04 18:51:33 -0500

    userJson minor fixes, add javadocs

--- src/pkglogin/UserJson.java
+++ src/pkglogin/UserJson.java
@@ -17,16 +17,27 @@ public class UserJson {
     private UserList userList;
     private final Gson gson;
     
+    /**
+     * Default constructor for UserJson class.
+     */
     public UserJson() {
         this.targetFile = "src/pkglogin/users.json";
         this.userList = new UserList();
         this.gson = new GsonBuilder().setPrettyPrinting().create();
     }
     
+    /**
+     * Reads from json file and fills user list.
+     * @throws FileNotFoundException If users.json does not exist.
+     */
     public void fromJson() throws FileNotFoundException {
         userList = gson.fromJson(new FileReader(targetFile), userList.getClass());
     }
     
+    /**
+     * Reads from user list and writes to json file.
+     * @throws FileNotFoundException If users.json does not exist.
+     */
     public void toJson() throws FileNotFoundException {
         String jsonString = gson.toJson(userList);
         try (PrintWriter writer = new PrintWriter(new FileOutputStream(targetFile, false))) {
@@ -34,6 +45,18 @@ public class UserJson {
         }
     }
     
+    /**
+     * Public method to retrieve user list.
+     * @return The list of users.
+     */
+    public UserList getUserList() {
+        return this.userList;
+    }
+    
+    /**
+     * Public method to add mock users to the user list (for testing purposes).
+     * @throws FileNotFoundException If users.json does not exist.
+     */
     public void addMockUsers() throws FileNotFoundException {
         User u1 = new User(00001, "Steve", "pass1");
         User u2 = new User(00002, "Thomas", "pass2");
--- src/pkglogin/UserList.java
+++ src/pkglogin/UserList.java
@@ -11,24 +11,43 @@ public class UserList {
     
     private final List<User> userList;
     
+    /**
+     * Default constructor for UserList class.
+     */
     public UserList() {
         this.userList = new ArrayList<>();
     }
     
+    /**
+     * Public method to add elements to the user list.
+     * @param user Object of User type to be added to the list.
+     */
     public void add(User user) {
         this.userList.add(user);
     }
     
+    /**
+     * Public method to retrieve User objects from the list.
+     * @param i Index of the target object.
+     * @return Chosen object of User type.
+     */
     public User get(int i) {
         return userList.get(i);
     }
     
+    /**
+     * Public method to print user list to console.
+     */
     public void printResults() {
         for (int i = 0; i < userList.size(); i++) {
             System.out.println(userList.get(i));
         }
     }
     
+    /**
+     * Override of toString method that gives the userList.
+     * @return List summary of userList.
+     */
     @Override
     public String toString() {
         String output = "[";
